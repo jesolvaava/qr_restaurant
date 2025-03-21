764d2037170bf5369e591a3ab34e10632f580ee9
jesolvaava <jesolpaul@gmail.com>
2025-03-21 11:37:33 +0530

    Update admin.py

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Table, Menu, Order, OrderItem,Staff
-from django.utils.safestring import mark_safe
 
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
@@ -8,29 +7,7 @@ class TableAdmin(admin.ModelAdmin):
 
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'description', 'image_preview']  # Add 'image_preview' to display in the list view
-    readonly_fields = ['image_preview']  # Add this to display the image preview in the admin panel
-
-    def image_preview(self, obj):
-        if obj.image_url:
-            return mark_safe(f'<img src="{obj.image_url}" width="150" height="150" />')
-        return "No Image"
-
-    image_preview.short_description = 'Image Preview'
-
-    def image_preview(self, obj):
-        if obj.image_url:
-            return mark_safe(f'<img src="{obj.image_url}" width="150" height="150" />')
-        return "No Image"
-
-    image_preview.short_description = 'Image Preview'
-
-    def image_preview(self, obj):
-        if obj.image:
-            return mark_safe(f'<img src="{obj.image.url}" width="150" height="150" />')
-        return "No Image"
-
-    image_preview.short_description = 'Image Preview'
+    list_display = ['name', 'price', 'description']
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
