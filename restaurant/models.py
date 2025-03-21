dfb481bbf20057064c208fd4750d72f862050889
jesolvaava <jesolpaul@gmail.com>
2025-03-21 11:18:28 +0530

    Update models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,6 +1,4 @@
-# restaurant/models.py
 from django.db import models
-from django.utils import timezone
 
 class Table(models.Model):
     table_number = models.IntegerField(unique=True)
@@ -8,12 +6,11 @@ class Table(models.Model):
     def __str__(self):
         return f"Table {self.table_number}"
 
-
 class Menu(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.CharField(max_length=500, blank=True, null=True)  # Store the image URL
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)  # Use ImageField instead of CloudinaryField
     is_in_stock = models.BooleanField(default=True)
 
     def __str__(self):
@@ -41,11 +38,11 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity}x {self.menu_item.name}"
-    
+
 class Staff(models.Model):
     username = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=100)  # Store hashed passwords in production
-    role = models.CharField(max_length=100, blank=True, null=True)  # Make role optional
+    password = models.CharField(max_length=100)
+    role = models.CharField(max_length=100, blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
