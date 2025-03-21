03e1ab339980f14c55a4f4bb8e097f0fe5719e89
jesolvaava <jesolpaul@gmail.com>
2025-03-21 10:04:47 +0530

    Update models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -12,14 +12,12 @@ class Menu(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
-    is_in_stock = models.BooleanField(default=True)  # Track stock status
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)  # Ensure this line is correct
+    is_in_stock = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
 
-    def __str__(self):
-        return self.name
 
 class Order(models.Model):
     table = models.ForeignKey('Table', on_delete=models.CASCADE)
