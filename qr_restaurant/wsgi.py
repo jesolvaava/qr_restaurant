2aa4f3ab021d0483f65836210135ed11b63a04b4
jesolvaava <jesolpaul@gmail.com>
2025-04-06 12:45:15 +0530

    Update wsgi.py

--- qr_restaurant/wsgi.py
+++ qr_restaurant/wsgi.py
@@ -1,28 +1,18 @@
-"""
-WSGI config for qr_restaurant project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+
+# Add this line to define BASE_DIR (should match your settings.py)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr_restaurant.settings')
 
 application = get_wsgi_application()
 
-# Create staticfiles directory if it doesn't exist
-static_root = os.path.join(os.path.dirname(__file__), 'staticfiles')
-os.makedirs(static_root, exist_ok=True)
-
+# Configure WhiteNoise
 application = WhiteNoise(
     application,
     root=os.path.join(BASE_DIR, 'staticfiles'),
-    max_age=31536000
+    max_age=31536000  # 1 year cache
 )
-application.add_files('/opt/render/project/src/media', prefix='/media/')
 application.add_files(os.path.join(BASE_DIR, 'media'), prefix='/media/')
