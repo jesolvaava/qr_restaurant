5975abfdb1a52e28431db007495f76e984619917
jesolvaava <jesolpaul@gmail.com>
2025-04-06 11:07:26 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ set -o errexit
 mkdir -p media
 mkdir -p staticfiles
 
+# Install exact versions from requirements.txt
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Django deployment steps
 python manage.py collectstatic --no-input
 python manage.py migrate
